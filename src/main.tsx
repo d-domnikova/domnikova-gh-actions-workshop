534a27c390d1c5bce736478e490b29ed73841e05
d-domnikova <138039813+d-domnikova@users.noreply.github.com>
2025-05-13 19:23:53 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { createGlobalStyle } from "styled-components";
 
+//comment for step 3.5 in the second part of GitHub Action Workshop 
 const Global = createGlobalStyle`
   body {
     background-image: url("gsfc_20171208_archive_e000226_orig-2.jpg");
